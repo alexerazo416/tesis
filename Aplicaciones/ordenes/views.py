d952182efc1b28f25e96d1c8037ca1928447335c
alexerazo416 <alinio-10@hotmail.com>
2024-08-19 15:06:59 -0500

    regreso

--- Aplicaciones/ordenes/views.py
+++ Aplicaciones/ordenes/views.py
@@ -21,9 +21,6 @@ from django.core.exceptions import PermissionDenied
 from .utils import role_required
 from django.db.models.deletion import ProtectedError
 from django.contrib.sessions.models import Session
-from django.contrib.auth import logout
-from .models import UserSession
-
 
 
 

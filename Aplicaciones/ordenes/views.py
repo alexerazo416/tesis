ea14702346c2825a3540141b0141c6c1add5eda1
alexerazo416 <alinio-10@hotmail.com>
2024-08-19 15:31:36 -0500

    importacion faltante

--- Aplicaciones/ordenes/views.py
+++ Aplicaciones/ordenes/views.py
@@ -21,7 +21,7 @@ from django.core.exceptions import PermissionDenied
 from .utils import role_required
 from django.db.models.deletion import ProtectedError
 from django.contrib.sessions.models import Session
-
+from django.contrib.auth.views import LogoutView
 
 
 
